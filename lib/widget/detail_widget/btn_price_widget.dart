b343fa51ceaaa121bd0c724fadf72ffcd42307d8
nurlan1998 <nurlan_98.ru@bk.ru>
2021-09-23 10:56:45 +0500

    добавлено seekbar и поправил дизайн bottomSheet

--- lib/widget/detail_widget/btn_price_widget.dart
+++ lib/widget/detail_widget/btn_price_widget.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:task_ui/widget/bottom_sheet_widget/bottom_sheet_widget.dart';
 
 //кнопки внизу скролла второй страницы
-class BtnPriceWidget extends StatefulWidget{
+class BtnPriceWidget extends StatefulWidget {
   const BtnPriceWidget({Key? key}) : super(key: key);
 
   @override
   _BtnPriceState createState() => _BtnPriceState();
 }
-class _BtnPriceState extends State<BtnPriceWidget>{
 
+class _BtnPriceState extends State<BtnPriceWidget> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -22,24 +22,19 @@ class _BtnPriceState extends State<BtnPriceWidget>{
             children: <Widget>[
               Expanded(
                   child: Container(
-                    height: 40,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        color: Colors.orangeAccent,
-                        border: Border.all(
-                            color: Colors.orange,
-                            width: 2
-                        )
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Icon(Icons.cached_sharp),
-                        Text("32.52")
-                      ],
-                    ),
-                  )
-              ),
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.orangeAccent,
+                    border: Border.all(color: Colors.orange, width: 2)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Icon(Icons.cached_sharp),
+                    Text("32.52")
+                  ],
+                ),
+              )),
               Expanded(
                   flex: 2,
                   child: Container(
@@ -47,32 +42,31 @@ class _BtnPriceState extends State<BtnPriceWidget>{
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(100),
                         color: Colors.blue,
-                        border: Border.all(
-                            color: Colors.blue,
-                            width: 2
-                        )
-                    ),
+                        border: Border.all(color: Colors.blue, width: 2)),
                     child: TextButton(
-                      onPressed: (){
+                      onPressed: () {
                         showModalBottomSheet(
+                            elevation: 128,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(32.0),
+                                  topLeft: Radius.circular(32.0)),
+                            ),
+                            backgroundColor: Colors.white,
                             context: context,
                             builder: (context) {
                               return const BottomSheetWidget();
                             });
-                        setState(() {
-
-                        });
+                        setState(() {});
                       },
                       child: const Text(
                         "Buy now",
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
-                  )
-              )
+                  ))
             ],
           ),
-        )
-    );
+        ));
   }
 }
