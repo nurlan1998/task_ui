2e9d998f8e75c290f7a5f9cd48d0045dc4c67b49
nurlan1998 <nurlan_98.ru@bk.ru>
2021-09-23 11:24:21 +0500

    задание Ui 2

---
+++ task_ui_2/lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatelessWidget{
+  const HomePageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Task Ui 2"),
+      )
+    );
+  }
+}
